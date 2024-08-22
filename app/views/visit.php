3216503e5db443b98a0a44c98a02b51415901481
micky-4u <laryeamichael4u@gmail.com>
2024-08-22 13:40:02 +0000

    vist property page

--- app/views/visit.php
+++ app/views/visit.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="public/css/styles.css">
     <link rel="stylesheet" href="public/css/navbar.css">
     <link rel="stylesheet" href="public/css/article.css">
+    <link rel="stylesheet" href="public/css/slider.css">
     <link rel="stylesheet" href="public/css/footer.css">
     <link rel="stylesheet" href="public/css/core.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
@@ -25,9 +26,9 @@
         <?php
             include("app/components/header.php");
         ?>
-        </div>
+    </div>
 
-        <div class="visit-content">
+    <div class="visit-content">
             <div>
                 <h1>
                 The MaGrip House
@@ -36,13 +37,20 @@
                 <p>Fast Growing Real Estate Company <br>that builds high earned properties at affordable prices.</p>
                 <hr>
             </div>
-            <div>
+
+        </div>
+        <div class ="slider-div">
+            <div class="slide fade">
+                <img src="public/images/prop2/img2.jpg" alt="">
+                <div class ="app-content">
+                    <h1>3 Bedroom Apartments</h1>
+                </div>
             </div>
         </div>
 
-
         </div>
     </div>
+
     <div class="footer">
         <?php
             include("app/components/footer.php");
